10fd0b81cc748e3b5d044f1a1d62e11846021ff1
MG <magallardo11@outlook.com>
2022-11-26 15:13:57 -0700

    added boilerplate server file changes.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// const {  }
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -11,7 +11,7 @@ const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: authMiddleware,
+    context: authMiddleware,
 });
 
 app.use(appress.urlencoded({ extended: false }))
