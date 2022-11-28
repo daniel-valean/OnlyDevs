a307655dbe25249c4ff3dd8a20c27ccee00bca39
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-11-28 15:07:17 -0700

    added client/src/utils and set up queries.js.  Working on mutations.js.

---
+++ client/src/utils/queries.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+// gets user information
+export const QUERY_USER = gql`
+query getUser {
+  getUser {
+    username
+    password
+    email
+    _id
+  }
+}
+`;
+// gets project information
+export const QUERY_PROJECT = gql`
+query GetProject($id: ID, $name: String) {
+  getProject(_id: $id, name: $name) {
+    _id
+    title
+    description
+    image
+    fundingGoal
+    fundingProgress
+    purpose
+    user {
+      _id
+      username
+      email
+      password
+    }
+    comments {
+      _id
+      comment
+      createdAt
+    }
+  }
+}
+`;
+// Gets all projects
+export const QUERY_ALL_PROJECTS = gql`
+query GetProjects {
+  getProjects {
+    _id
+    title
+    description
+    image
+    fundingGoal
+    fundingProgress
+    purpose
+    user {
+      _id
+      username
+      email
+      password
+    }
+    comments {
+      _id
+      comment
+      createdAt
+    }
+  }
+}
+`;
