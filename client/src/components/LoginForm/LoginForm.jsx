2235733fdcd3366ad100cf371f134e613c564b2d
MG <magallardo11@outlook.com>
2022-11-29 10:12:52 -0700

    reverted loginform changes to avoid merge conflicts.

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,8 @@
 import './LoginForm.css'
 import { FormControl, FormLabel, Input, Text, InputGroup, Button, InputRightElement} from '@chakra-ui/react'
 import {useState} from 'react'
-import { useMutation } from '@apollo/client';
-import {LOGIN } from '../utils/mutations';
 
 export default function LoginForm() {
-    const [login, {error}] = useMutation(LOGIN)
     const [showPassword, setShowPassword] = useState(false)
     const handleShowPassword = () => setShowPassword(!showPassword)
 
@@ -27,19 +24,8 @@ export default function LoginForm() {
     async function handleSubmit(e) {
         e.preventDefault();
         console.log(username, password);
-        try {
-            const data = await login({
-                variables: { 
-                    username,
-                    password
-                }
-            })
-            Auth.login(data.login.token);
-            setUsername("");
-            setPassword("");
-        } catch(err) {
-            console.error(err)
-        }
+        setUsername("");
+        setPassword("");
     }
 
     return (
