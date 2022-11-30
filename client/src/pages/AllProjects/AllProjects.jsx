c690ad84b045b463ea6ce302f9e606db06f14f0f
Toacin <toacinp@outlook.com>
2022-11-29 23:17:05 -0700

    Updated buttons on all projects page to be wrapped in link rather than use OnClick call back function window re-assignment

--- client/src/pages/AllProjects/AllProjects.jsx
+++ client/src/pages/AllProjects/AllProjects.jsx
@@ -3,19 +3,20 @@ import Header from '../../components/Header/Header'
 import { Card, CardBody, Image, Stack, Heading, Text, Divider, CardFooter, ButtonGroup, Button, SimpleGrid, CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_PROJECTS } from '../../utils/queries';
+import { Link } from 'react-router-dom';
 
 export default function AllProject() {
     const { data, loading } = useQuery(QUERY_ALL_PROJECTS);
     console.log(data);
 
     if (loading) {
-        return <div className='project-display-wrapper'><CircularProgress isIndeterminate color='green.300' /></div>
+        return <div className='project-display-wrapper'><CircularProgress isIndeterminate color='#05d5f4' /></div>
     } else {
         return (
             <>
                 <Header></Header>
                 <div className='all-project-display-wrapper'>
-                    <Heading fontSize='6xl' color="#05d5f4" marginBottom="50px">Projects</Heading>
+                    <Heading fontSize='6xl' color="#484a4a" marginBottom="50px">Projects</Heading>
                     <SimpleGrid columns={4} spacing={10}>
                         {data.getProjects.map((project) => (
                             <Card key={project._id} maxW='xs'>
@@ -33,19 +34,15 @@ export default function AllProject() {
                                 <Divider />
                                 <CardFooter>
                                     <ButtonGroup spacing='2' display="flex" alignContent="center" width="100%">
-                                        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%"}}>
-                                            <Button
-                                                onClick={() => {
-                                                    window.location.assign(`/project-display/${project._id}`)
-                                                }}
-                                                variant='solid'
-                                                colorScheme='blue'
-                                                bg="#05d5f4"
-                                            >
-                                                View
-                                            </Button>
-                                            <CircularProgress value={(project.fundingProgress/project.fundingGoal)*100} color='#05d5f4'>
-                                                <CircularProgressLabel>{(project.fundingProgress/project.fundingGoal)*100}%</CircularProgressLabel>
+                                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+
+                                            <Link to={`/project-display/${project._id}`}>
+                                                <Button variant='solid' colorScheme="blue" bg="#05d5f4" color="white">
+                                                    View
+                                                </Button>
+                                            </Link>
+                                            <CircularProgress value={((project.fundingProgress / project.fundingGoal) * 100 > 100) ? 100 : (project.fundingProgress / project.fundingGoal) * 100} color='#05d5f4'>
+                                                <CircularProgressLabel>{((project.fundingProgress / project.fundingGoal) * 100 > 100) ? 100 : Math.floor((project.fundingProgress / project.fundingGoal) * 100)}%</CircularProgressLabel>
                                             </CircularProgress>
                                         </div>
                                     </ButtonGroup>
